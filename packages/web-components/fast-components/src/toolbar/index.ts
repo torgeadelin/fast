17cc1d97dd17a38dc76fa3a298b329483d473cfd
John Kreitlow <john.kreitlow@microsoft.com>
2021-04-02 18:29:58 -0700

    add component scenario and definition for toolbar

--- packages/web-components/fast-components/src/toolbar/index.ts
+++ packages/web-components/fast-components/src/toolbar/index.ts
@@ -2,6 +2,15 @@ import { customElement } from "@microsoft/fast-element";
 import { Toolbar, ToolbarTemplate } from "@microsoft/fast-foundation";
 import { ToolbarStyles } from "./toolbar.styles";
 
+/**
+ * The FAST toolbar Custom Element. Implements {@link @microsoft/fast-foundation#Toolbar},
+ * {@link @microsoft/fast-foundation#ToolbarTemplate}
+ *
+ * @public
+ * @remarks
+ * HTML Element: `<fast-toolbar>`
+ *
+ */
 @customElement({
     name: "fast-toolbar",
     template: ToolbarTemplate,
@@ -9,4 +18,8 @@ import { ToolbarStyles } from "./toolbar.styles";
 })
 export class FASTToolbar extends Toolbar {}
 
+/**
+ * Styles for Toolbar.
+ * @public
+ */
 export { ToolbarStyles };
